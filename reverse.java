533e2cc03fe717fde3b70ed1118415993040e421
MONISHKUMAR R <160628695+Monishkumar15@users.noreply.github.com>
2025-03-19 10:10:53 +0530

    Java reverse program
    this porgram reveresed on string

---
+++ reverse.java
@@ -0,0 +1,23 @@
+public class reverse {
+    public static void main(String args[])
+    {
+        int a[]={1,2,3},j=0;
+        int c[]=new int[3];
+
+        for (int i=2;i>=0;i--){
+            if(j<3){
+                c[j]=a[i];
+                j+=1;
+               
+            }
+           
+           }
+        /*for(int i=2,j=0;i>=0;i--,j++){
+            c[j]=a[i];
+        }*/
+        for (int k=0;k<3;k++){
+        System.out.print(c[k]+" ");
+        }
+
+    }
+}
